53d5c3167a3591059f6ce0b4447dd88a78e5a531
Zane D. Purvis <zane.purvis@gmail.com>
2022-12-23 15:47:27 -0500

    Day 10, part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -54,16 +54,34 @@ fn main() {
     let mut signal_strength_sum = 0;
     let mut instructions = instructions.into_iter();
     let mut cur_inst = instructions.next().unwrap();
+
+    let mut cur_crt_line: Vec<char> = Vec::new();
     loop {
-        println!("@{}, inst={:?}, X={}", cycle, cur_inst, x);
+        // println!("@{}, inst={:?}, X={}", cycle, cur_inst, x);
         match cycle {
             20 | 60 | 100 | 140 | 180 | 220 => {
                 let signal_strength = cycle * x;
-                println!("@{cycle}, signal strength = {signal_strength}");
+                // println!("@{cycle}, signal strength = {signal_strength}");
                 signal_strength_sum += signal_strength;
             }
             _ => {}
         }
+
+        let cur_pixel: i32 = (cycle - 1) % 40;
+        if cur_pixel == 0 {
+            println!("{}", cur_crt_line.iter().collect::<String>());
+            cur_crt_line = Vec::new();
+        }
+        if cur_pixel.abs_diff(x) <= 1 {
+            //print!("cycle {}", cycle);
+            cur_crt_line.push('#');
+        } else {
+            cur_crt_line.push('.');
+        }
+        // println!(
+        //     "@{cycle}/{cur_pixel}: ({x}) {}",
+        //     cur_crt_line.iter().collect::<String>()
+        // );
 
         cur_inst.cycles_remaining -= 1;
         if cur_inst.cycles_remaining == 0 {
@@ -73,7 +91,15 @@ fn main() {
             if let Some(next_inst) = instructions.next() {
                 cur_inst = next_inst;
             } else {
-                break;
+                cur_inst = Instruction {
+                    opcode: Noop,
+                    cycles_remaining: 1,
+                    ..Instruction::default()
+                };
+                if cycle > 260 {
+                    break;
+                }
+                //break;
             }
         }
         cycle += 1;
